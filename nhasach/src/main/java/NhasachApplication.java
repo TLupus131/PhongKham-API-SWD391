8b45c5dda74891a781336fdda54b494a802bfed0
TLupus131 <hahacondien@gmail.com>
2024-01-25 00:48:26 +0700

    init project

---
+++ nhasach/src/main/java/NhasachApplication.java
@@ -0,0 +1,13 @@
+package com.nole.nhasach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhasachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhasachApplication.class, args);
+	}
+
+}
---
+++ nhasach/src/test/java/com/nole/nhasach/NhasachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nole.nhasach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhasachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
